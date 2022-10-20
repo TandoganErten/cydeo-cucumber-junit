68a05b1f840fd7ae75269b23c01f2fea2355b4d6
TandoganErten <tereten0706@gmail.com>
2022-10-21 01:25:48 +0300

    another commit for update

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
 features = "src/test/resources/features",
         glue ="com/cydeo/step_definitions",
         dryRun =false,
-        tags = "@wip3"
+        tags = "@smoke"
        //  , publish = true
 )
 public class CukesRunner {
